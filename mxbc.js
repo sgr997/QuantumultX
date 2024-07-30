34112837f9a5c16102a3bafc5ac6e97cf9ef6513
pei <51942582+sgr997@users.noreply.github.com>
2024-07-30 10:59:17 +0800

    更新 mxbc.js

--- mxbc.js
+++ mxbc.js
@@ -30,7 +30,7 @@ const $ = new Env('mxbc');
         $.mxbc_cc = $.getdata('mxbc_cc', '11:00')
         $.mxbc_word = $.getdata('mxbc_word', '')
         $.mxbc_wait_time = $.getdata('mxbc_wait_time', 500)
-        let word = await getword()
+        //let word = await getword()
         for (let i = 0; i < $.mxbc_times; i++) {
             let now = Date.now()
             let param = `marketingId=1816854086004391938&round=${$.mxbc_cc}&s=2&secretword=${$.mxbc_word}&stamp=${now}c274bac6493544b89d9c4f9d8d542b84`
